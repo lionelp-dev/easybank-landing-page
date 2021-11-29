504a8da443c346c36e4c13b8061a1e61f439b232
lionelp.dev <lionelp.dev@gmail.com>
2021-11-29 10:01:52 +0100

    feat(): integration of the design

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
-import './style.scss'
+window.addEventListener("load", () => {
+  const buttonMenu = document.querySelector(".header__menu");
+  const menu = document.querySelector(".header__links");
+  const body = document.querySelector("body");
 
+  buttonMenu?.addEventListener("click", () => {
+    if (buttonMenu.getAttribute("aria-expanded") === "true") {
+      body?.classList.remove("noscroll");
+      menu?.setAttribute("data-visible", "false");
+      buttonMenu?.setAttribute("aria-expanded", "false");
+    } else if (buttonMenu.getAttribute("aria-expanded") === "false") {
+      body?.classList.add("noscroll");
+      menu?.setAttribute("data-visible", "true");
+      buttonMenu?.setAttribute("aria-expanded", "true");
+    }
+  });
+});
